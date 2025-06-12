54549fbab40231608020e969c51bc9f6e44213e3
Lucas Camilo <lucas.camilo@harmonit.com.br>
2025-06-12 11:34:23 -0300

    fix: dependency injection

--- ProjetosApi/Ioc/IndependencyInjection.cs
+++ ProjetosApi/Ioc/IndependencyInjection.cs
@@ -6,21 +6,54 @@ using Application.Interfaces;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using Refit;
-using System.IdentityModel.Tokens.Jwt;
 using System.Text;
 using Microsoft.OpenApi.Models;
 using Application.Interfaces.OAuth2;
 using Application.Services.OAuth2;
 
+
 namespace ProjetosApi.Ioc
 {
     public static class IndependencyInjection
     {
-        public static void AddSwaggerConfiguration(this IServiceCollection services)
+        public static IServiceCollection ServiceCollection { get; private set; } = new ServiceCollection();
+        public static IConfiguration? Configuration { get; private set; }
+        public static IServiceCollection ConfigureDependencyInjection(this IServiceCollection serviceCollection, IConfiguration configuration)
         {
-            services.AddControllers();
-            services.AddEndpointsApiExplorer();
-            services.AddSwaggerGen(options =>
+            ServiceCollection = serviceCollection;
+            Configuration = configuration;
+            AddSwaggerConfiguration();
+            AddScopedServices();
+            AddRefitSettings();
+            AddOAuth2();
+            serviceCollection.AddMemoryCache();
+            return serviceCollection;
+        }
+        public static void AddSwaggerConfiguration()
+        {
+            ServiceCollection.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes("A2D8u9jWwWnKlQ27@pN7XzL5bQyP9v3l2cT5y0jZf8yM1mZ2tG7uO0c3QhVwD9bA")),
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
+
+            ServiceCollection.AddControllers();
+            ServiceCollection.AddEndpointsApiExplorer();
+
+            ServiceCollection.AddSwaggerGen(options =>
             {
                 options.SwaggerDoc("v1", new OpenApiInfo { Title = "Projeto integração", Version = "v1" });
 
@@ -36,43 +69,43 @@ namespace ProjetosApi.Ioc
 
                 options.AddSecurityRequirement(new OpenApiSecurityRequirement
                 {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
                     {
-                        new OpenApiSecurityScheme
-                        {
-                            Reference = new OpenApiReference
-                            {
-                                Type = ReferenceType.SecurityScheme,
-                                Id = "Bearer"
-                            }
-                        },
-                        new string[] {}
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
                     }
+                },
+                new string[] { }
+            }
                 });
             });
         }
-        public static void AddScopedServices(this IServiceCollection services)
+        public static void AddScopedServices()
         {
-            services.AddScoped<ICepService, CepService>();
-            services.AddScoped<IFipeService, FipeService>();
-            services.AddScoped<IIbgeService, IbgeService>();
-            services.AddScoped<IAuthService, AuthService>();
+            ServiceCollection.AddScoped<ICepService, CepService>();
+            ServiceCollection.AddScoped<IFipeService, FipeService>();
+            ServiceCollection.AddScoped<IIbgeService, IbgeService>();
+            ServiceCollection.AddScoped<IAuthService, AuthService>();
         }
-        public static void AddRefitSettings(this IServiceCollection services)
+        public static void AddRefitSettings()
         {
-            services.AddRefitClient<ICepRefit>().ConfigureHttpClient(c =>
+            ServiceCollection.AddRefitClient<ICepRefit>().ConfigureHttpClient(c =>
+            {
+                c.BaseAddress = new Uri("https://brasilapi.com.br");
+            });/* .AddHttpMessageHandler<HttpRequestHandler>(); */
+            ServiceCollection.AddRefitClient<IFipeRefit>().ConfigureHttpClient(c =>
             {
                 c.BaseAddress = new Uri("https://brasilapi.com.br");
             });
-            services.AddRefitClient<IFipeRefit>().ConfigureHttpClient(c =>
-            {
-                c.BaseAddress = new Uri("https://brasilapi.com.br");
-            });
-            services.AddRefitClient<IIbgeRefit>().ConfigureHttpClient(c =>
+            ServiceCollection.AddRefitClient<IIbgeRefit>().ConfigureHttpClient(c =>
             {
                 c.BaseAddress = new Uri("https://brasilapi.com.br");
             });
         }
-        public static void AddToken(this IServiceCollection services)
+        /* public static void AddToken(this IServiceCollection services)
         {
             JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
 
@@ -92,11 +125,11 @@ namespace ProjetosApi.Ioc
                     ClockSkew = TimeSpan.Zero
                 };
             });
-        }
-        public static void AddOAuth2(this IServiceCollection services)
+        } */
+        public static void AddOAuth2()
         {
-            services.AddHttpClient();
-            services.AddScoped<IGoogleAuthService, GoogleAuthService>();
+            ServiceCollection.AddHttpClient();
+            ServiceCollection.AddScoped<IGoogleAuthService, GoogleAuthService>();
 
         }
     }
