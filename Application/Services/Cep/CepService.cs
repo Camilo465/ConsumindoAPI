76bbac7a2e974f61473e044ecdfaa4d5c949eb71
Lucas Camilo <lucas.camilo@harmonit.com.br>
2025-06-12 11:33:55 -0300

    feat: validações de retorno

--- Application/Services/Cep/CepService.cs
+++ Application/Services/Cep/CepService.cs
@@ -10,7 +10,28 @@ namespace Application.Interfaces.Entities
         public async Task<BaseApiResponse<OutputByPostalCode, string>> GetByPostalCode(string postalCode)
         {
             var response = await _refit!.GetByPostalCode(postalCode);
+
+            if (string.IsNullOrWhiteSpace(response.Content) || !IsJson(response.Content))
+            {
+                return ReturnError<OutputByPostalCode>("Error, Invalid Postal Code or unexpected response format");
+            }
+
             return ReturnResponse<OutputByPostalCode, string>(response);
+        }
+        private bool IsJson(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+                return false;
+
+            input = input.Trim();
+            return (input.StartsWith("{") && input.EndsWith("}")) || (input.StartsWith("[") && input.EndsWith("]"));
+        }
+        private BaseApiResponse<TTypeResult, string> ReturnError<TTypeResult>(string errorMessage)
+        {
+            return new BaseApiResponse<TTypeResult, string>
+            {
+                Error = errorMessage
+            };
         }
     }
 }
